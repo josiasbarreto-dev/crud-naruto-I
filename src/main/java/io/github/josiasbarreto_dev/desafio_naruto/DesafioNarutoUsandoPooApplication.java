a140007a08b3ad9e814f7a9c603d51a406c680db
Josias Barreto <sr.josiasbarreto@gmail.com>
2025-06-23 19:54:57 -0300

    Initial project setup

---
+++ src/main/java/io/github/josiasbarreto_dev/desafio_naruto/DesafioNarutoUsandoPooApplication.java
@@ -0,0 +1,13 @@
+package io.github.josiasbarreto_dev.desafio_naruto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioNarutoUsandoPooApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioNarutoUsandoPooApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/josiasbarreto_dev/desafio_naruto/DesafioNarutoUsandoPooApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.josiasbarreto_dev.desafio_naruto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioNarutoUsandoPooApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
