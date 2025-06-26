64e0a4f0e4fd0ca0c8d2f4a34be156dd1af7e102
Josias Barreto <sr.josiasbarreto@gmail.com>
2025-06-26 12:45:58 -0300

    feat: Adds DTOs to be later converted into entities or DTO Responses.

---
+++ src/main/java/io/github/josiasbarreto_dev/desafio_naruto/dto/ChakraRequestDTO.java
@@ -0,0 +1,6 @@
+package io.github.josiasbarreto_dev.desafio_naruto.dto;
+
+import io.github.josiasbarreto_dev.desafio_naruto.model.NinjaType;
+
+public record ChakraRequestDTO(int chakraValue, NinjaType ninjaType) {
+}
