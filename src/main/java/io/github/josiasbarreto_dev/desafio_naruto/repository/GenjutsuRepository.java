02ed0e0a09aed356956f7f2a269399c81c273717
Josias Barreto <sr.josiasbarreto@gmail.com>
2025-07-09 14:55:15 -0300

    refactor: Removal of individual repositories for each ninja to use a single repository through the "CharacterRepository" using the "DiscriminatorColumn" annotation to distinguish ninja types within the same table.

--- src/main/java/io/github/josiasbarreto_dev/desafio_naruto/repository/GenjutsuRepository.java
+++
@@ -1,6 +0,0 @@
-package io.github.josiasbarreto_dev.desafio_naruto.repository;
-import io.github.josiasbarreto_dev.desafio_naruto.model.GenjutsuNinja;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface GenjutsuRepository extends JpaRepository<GenjutsuNinja, Long>{
-}
--- src/main/java/io/github/josiasbarreto_dev/desafio_naruto/repository/NinjutsuRepository.java
+++
@@ -1,6 +0,0 @@
-package io.github.josiasbarreto_dev.desafio_naruto.repository;
-import io.github.josiasbarreto_dev.desafio_naruto.model.NinjutsuNinja;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface NinjutsuRepository extends JpaRepository<NinjutsuNinja, Long>{
-}
