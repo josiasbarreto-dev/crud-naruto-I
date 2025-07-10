02f9b724a1eacee53a69df8e7b9137b54fb84c7e
Josias Barreto <sr.josiasbarreto@gmail.com>
2025-07-10 13:48:13 -0300

    refactor: Modifies the request character's attributes following the challenge requirements.

--- src/main/java/io/github/josiasbarreto_dev/desafio_naruto/dto/CharacterRequestDTO.java
+++ src/main/java/io/github/josiasbarreto_dev/desafio_naruto/dto/CharacterRequestDTO.java
@@ -1,13 +1,13 @@
 package io.github.josiasbarreto_dev.desafio_naruto.dto;
 
+import io.github.josiasbarreto_dev.desafio_naruto.model.Jutsu;
 import io.github.josiasbarreto_dev.desafio_naruto.model.NinjaType;
 
-import java.util.ArrayList;
+import java.util.Map;
 
 public record CharacterRequestDTO(
         String name,
-        int age,
-        String village,
-        ArrayList<String> jutsus,
-        int chakra,
-        NinjaType ninjaType) {}
+        Map<String, JutsuRequestDTO> jutsus,
+        int life,
+        NinjaType ninjaType
+) {}
