36cd7f3c57f50bcfa2b9d1806aa722f7bc8d2514
Josias Barreto <sr.josiasbarreto@gmail.com>
2025-07-10 15:56:22 -0300

    refactor: Adds @Override annotation to implemented methods of the interface.

--- src/main/java/io/github/josiasbarreto_dev/desafio_naruto/controller/impl/CharacterController.java
+++ src/main/java/io/github/josiasbarreto_dev/desafio_naruto/controller/impl/CharacterController.java
@@ -23,36 +23,43 @@ public class CharacterController implements CharacterControllerInterface {
         this.characterService = characterService;
     }
 
+    @Override
     public ResponseEntity<CharacterResponseDTO> createCharacter(@RequestBody CharacterRequestDTO requestDTO) {
         var character = characterService.createCharacter(requestDTO);
         return ResponseEntity.status(HttpStatus.CREATED).body(character);
     }
 
+    @Override
     public ResponseEntity<List<CharacterResponseDTO>> listCharacter() {
         var listCharacter = characterService.listCharacter();
         return ResponseEntity.ok(listCharacter);
     }
 
+    @Override
     public ResponseEntity<Character> getCharacterById(@PathVariable Long characterId) {
         var characterInfo = characterService.getCharacterById(characterId);
         return ResponseEntity.ok(characterInfo);
     }
 
+    @Override
     public ResponseEntity<List<CharacterResponseDTO>> listCharactersByType(@RequestParam NinjaType ninjaType){
         var listOfCharacterTypes = characterService.listCharactersByType(ninjaType);
         return ResponseEntity.ok(listOfCharacterTypes);
     }
 
+    @Override
     public ResponseEntity<Void> deleteCharacterById(@PathVariable Long characterId){
         characterService.deleteCharacterById(characterId);
         return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
     }
 
+    @Override
     public ResponseEntity<BattleResponseDTO> fight(@RequestBody AttackRequestDTO attackRequestDTO){
         var attackResult = characterService.fight(attackRequestDTO);
         return ResponseEntity.status(HttpStatus.OK).body(attackResult);
     }
 
+    @Override
     public ResponseEntity<CharacterResponseDTO> addChakra(@PathVariable Long ninjaId, @RequestParam int chakraAmount){
         var character = characterService.addChakra(ninjaId, chakraAmount);
         return ResponseEntity.status(HttpStatus.CREATED).body(character);
