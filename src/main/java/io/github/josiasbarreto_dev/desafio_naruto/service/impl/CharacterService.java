a464c6aaa8781baf52d7bcd0e6407259376f9ad0
Josias Barreto <sr.josiasbarreto@gmail.com>
2025-07-10 15:54:31 -0300

    refactor: Adds @Override annotation to implemented methods of the interface.

--- src/main/java/io/github/josiasbarreto_dev/desafio_naruto/service/impl/CharacterService.java
+++ src/main/java/io/github/josiasbarreto_dev/desafio_naruto/service/impl/CharacterService.java
@@ -11,7 +11,6 @@ import io.github.josiasbarreto_dev.desafio_naruto.model.*;
 import io.github.josiasbarreto_dev.desafio_naruto.model.Character;
 import io.github.josiasbarreto_dev.desafio_naruto.repository.CharacterRepository;
 import io.github.josiasbarreto_dev.desafio_naruto.service.CharacterServiceInterface;
-import jakarta.transaction.Transactional;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -26,7 +25,7 @@ public class CharacterService implements CharacterServiceInterface {
         this.mapper = mapper;
     }
 
-    @Transactional
+    @Override
     public CharacterResponseDTO createCharacter(CharacterRequestDTO requestDTO) {
         if (repository.existsByName(requestDTO.name())) {
             throw new NameAlreadyExistsException("There is already a registered character with this name: " + requestDTO.name());
@@ -47,15 +46,18 @@ public class CharacterService implements CharacterServiceInterface {
         return mapper.toDTO(repository.save(character));
     }
 
+    @Override
     public List<CharacterResponseDTO> listCharacter() {
         var listCharacter = repository.findAll();
         return mapper.toDTO(listCharacter);
     }
 
+    @Override
     public Character getCharacterById(Long characterId) {
         return repository.findById(characterId).orElseThrow(() -> new ResourceNotFoundException("Ninja with id " + characterId + " not found."));
     }
 
+    @Override
     public List<CharacterResponseDTO> listCharactersByType(NinjaType ninjaType) {
         return repository.findAll().stream().filter(character -> {
             return switch (ninjaType) {
@@ -66,12 +68,13 @@ public class CharacterService implements CharacterServiceInterface {
         }).map(mapper::toDTO).toList();
     }
 
+    @Override
     public void deleteCharacterById(Long characterId) {
         var character = repository.findById(characterId).orElseThrow(() -> new ResourceNotFoundException("Ninja with id " + characterId + " not found."));
         repository.delete(character);
     }
 
-    @Transactional
+    @Override
     public BattleResponseDTO fight(AttackRequestDTO dto) {
         var attacker = repository.findByName(dto.attacker()).orElseThrow(() -> new ResourceNotFoundException("Ninja with name " + dto.attacker() + " not found."));
         var target = repository.findByName(dto.target()).orElseThrow(() -> new ResourceNotFoundException("Ninja with name " + dto.target() + " not found."));
@@ -84,6 +87,7 @@ public class CharacterService implements CharacterServiceInterface {
         return new BattleResponseDTO(mapper.toDTO(attacker), mapper.toDTO(target));
     }
 
+    @Override
     public CharacterResponseDTO addChakra(Long ninjaId, int chakraAmount) {
         var character = getCharacterById(ninjaId);
         character.setChakra(chakraAmount);
