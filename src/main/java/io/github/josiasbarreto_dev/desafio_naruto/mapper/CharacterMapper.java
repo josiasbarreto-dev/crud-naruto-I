e33d1f0e3d567f2f8797c2286ec049b1a9c15fea
Josias Barreto <sr.josiasbarreto@gmail.com>
2025-07-10 13:36:45 -0300

    refactor: Modifies the CharacterMapper interface to convert any extended object of the character class to ResponseDTO and adds a new mapper to convert jutsu.

--- src/main/java/io/github/josiasbarreto_dev/desafio_naruto/mapper/CharacterMapper.java
+++ src/main/java/io/github/josiasbarreto_dev/desafio_naruto/mapper/CharacterMapper.java
@@ -1,26 +1,21 @@
 package io.github.josiasbarreto_dev.desafio_naruto.mapper;
 
-import io.github.josiasbarreto_dev.desafio_naruto.dto.CharacterRequestDTO;
 import io.github.josiasbarreto_dev.desafio_naruto.dto.CharacterResponseDTO;
-import io.github.josiasbarreto_dev.desafio_naruto.model.GenjutsuNinja;
-import io.github.josiasbarreto_dev.desafio_naruto.model.NinjutsuNinja;
-import io.github.josiasbarreto_dev.desafio_naruto.model.TaijutsuNinja;
+import io.github.josiasbarreto_dev.desafio_naruto.dto.JutsuRequestDTO;
+import io.github.josiasbarreto_dev.desafio_naruto.model.Character;
+import io.github.josiasbarreto_dev.desafio_naruto.model.Jutsu;
 import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
+
+import java.util.List;
 
 @Mapper(componentModel = "spring")
 public interface CharacterMapper {
     CharacterMapper INSTANCE = Mappers.getMapper(CharacterMapper.class);
 
-    @Mapping(target = "id", ignore = true)
-    GenjutsuNinja toEntityGenjutsu(CharacterRequestDTO requestDTO);
-    @Mapping(target = "id", ignore = true)
-    NinjutsuNinja toEntityNinjutsu(CharacterRequestDTO requestDTO);
-    @Mapping(target = "id", ignore = true)
-    TaijutsuNinja toEntityTaijutsu(CharacterRequestDTO requestDTO);
+    CharacterResponseDTO toDTO(Character character);
+    List<CharacterResponseDTO> toDTO(List<Character> characters);
 
-    CharacterResponseDTO toDTO(GenjutsuNinja entityGenjutsu);
-    CharacterResponseDTO toDTO(NinjutsuNinja entityNinjutsu);
-    CharacterResponseDTO toDTO(TaijutsuNinja entityTaijutsu);
+    Jutsu toEntity(JutsuRequestDTO jutsuRequestDTO);
+    JutsuRequestDTO toDTO(Jutsu jutsu);
 }
