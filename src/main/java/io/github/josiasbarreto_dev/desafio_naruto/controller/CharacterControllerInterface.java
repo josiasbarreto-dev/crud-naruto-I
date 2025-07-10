bbdd641627403f5de2492dc0c395c4d009e381f2
Josias Barreto <sr.josiasbarreto@gmail.com>
2025-07-10 15:11:19 -0300

    refactor: Create an interface containing all the controller methods and implement it in the impl package in order to make the class cleaner.

---
+++ src/main/java/io/github/josiasbarreto_dev/desafio_naruto/controller/CharacterControllerInterface.java
@@ -0,0 +1,37 @@
+package io.github.josiasbarreto_dev.desafio_naruto.controller;
+
+import io.github.josiasbarreto_dev.desafio_naruto.dto.AttackRequestDTO;
+import io.github.josiasbarreto_dev.desafio_naruto.dto.BattleResponseDTO;
+import io.github.josiasbarreto_dev.desafio_naruto.dto.CharacterRequestDTO;
+import io.github.josiasbarreto_dev.desafio_naruto.dto.CharacterResponseDTO;
+import io.github.josiasbarreto_dev.desafio_naruto.model.Character;
+import io.github.josiasbarreto_dev.desafio_naruto.model.NinjaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/character")
+public interface CharacterControllerInterface {
+    @PostMapping
+    public ResponseEntity<CharacterResponseDTO> createCharacter(@RequestBody CharacterRequestDTO requestDTO);
+
+    @GetMapping
+    public ResponseEntity<List<CharacterResponseDTO>> listCharacter();
+
+    @GetMapping("/{characterId}")
+    public ResponseEntity<Character> getCharacterById(@PathVariable Long characterId);
+
+    @GetMapping("/type")
+    public ResponseEntity<List<CharacterResponseDTO>> listCharactersByType(@RequestParam NinjaType ninjaType);
+
+    @DeleteMapping("/{characterId}")
+    public ResponseEntity<Void> deleteCharacterById(@PathVariable Long characterId);
+
+    @PostMapping("/battle")
+    ResponseEntity<BattleResponseDTO> fight(@RequestBody AttackRequestDTO attackRequestDTO);
+
+    @PostMapping("/chakra/{ninjaId}")
+    ResponseEntity<CharacterResponseDTO> addChakra(@PathVariable Long ninjaId, @RequestParam int chakraAmount);
+}
--- src/main/java/io/github/josiasbarreto_dev/desafio_naruto/controller/impl/CharacterController.java
+++ src/main/java/io/github/josiasbarreto_dev/desafio_naruto/controller/impl/CharacterController.java
@@ -1,62 +1,59 @@
-package io.github.josiasbarreto_dev.desafio_naruto.controller;
+package io.github.josiasbarreto_dev.desafio_naruto.controller.impl;
 
-import io.github.josiasbarreto_dev.desafio_naruto.dto.*;
+import io.github.josiasbarreto_dev.desafio_naruto.controller.CharacterControllerInterface;
+import io.github.josiasbarreto_dev.desafio_naruto.dto.AttackRequestDTO;
+import io.github.josiasbarreto_dev.desafio_naruto.dto.BattleResponseDTO;
+import io.github.josiasbarreto_dev.desafio_naruto.dto.CharacterRequestDTO;
+import io.github.josiasbarreto_dev.desafio_naruto.dto.CharacterResponseDTO;
 import io.github.josiasbarreto_dev.desafio_naruto.model.Character;
 import io.github.josiasbarreto_dev.desafio_naruto.model.NinjaType;
-import io.github.josiasbarreto_dev.desafio_naruto.service.CharacterService;
+import io.github.josiasbarreto_dev.desafio_naruto.service.impl.CharacterService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
-@RestController
-@RequestMapping("/character")
-public class CharacterController {
+public class CharacterController implements CharacterControllerInterface {
     private final CharacterService characterService;
 
     public CharacterController(CharacterService characterService) {
         this.characterService = characterService;
     }
 
-    @PostMapping
     public ResponseEntity<CharacterResponseDTO> createCharacter(@RequestBody CharacterRequestDTO requestDTO) {
         var character = characterService.createCharacter(requestDTO);
         return ResponseEntity.status(HttpStatus.CREATED).body(character);
     }
 
-    @GetMapping
     public ResponseEntity<List<CharacterResponseDTO>> listCharacter() {
         var listCharacter = characterService.listCharacter();
         return ResponseEntity.ok(listCharacter);
     }
 
-    @GetMapping("/{characterId}")
     public ResponseEntity<Character> getCharacterById(@PathVariable Long characterId) {
         var characterInfo = characterService.getCharacterById(characterId);
         return ResponseEntity.ok(characterInfo);
     }
 
-    @GetMapping("/type")
     public ResponseEntity<List<CharacterResponseDTO>> listCharactersByType(@RequestParam NinjaType ninjaType){
         var listOfCharacterTypes = characterService.listCharactersByType(ninjaType);
         return ResponseEntity.ok(listOfCharacterTypes);
     }
 
-    @DeleteMapping("/{characterId}")
     public ResponseEntity<Void> deleteCharacterById(@PathVariable Long characterId){
         characterService.deleteCharacterById(characterId);
         return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
     }
 
-    @PostMapping("/battle")
-    ResponseEntity<BattleResponseDTO> fight(@RequestBody AttackRequestDTO attackRequestDTO){
+    public ResponseEntity<BattleResponseDTO> fight(@RequestBody AttackRequestDTO attackRequestDTO){
         var attackResult = characterService.fight(attackRequestDTO);
         return ResponseEntity.status(HttpStatus.OK).body(attackResult);
     }
 
-    @PostMapping("/chakra/{ninjaId}")
-    ResponseEntity<CharacterResponseDTO> addChakra(@PathVariable Long ninjaId, @RequestParam int chakraAmount){
+    public ResponseEntity<CharacterResponseDTO> addChakra(@PathVariable Long ninjaId, @RequestParam int chakraAmount){
         var character = characterService.addChakra(ninjaId, chakraAmount);
         return ResponseEntity.status(HttpStatus.CREATED).body(character);
     }
