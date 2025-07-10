d815cf777ccccea54188c6c769a681019d7c81da
Josias Barreto <sr.josiasbarreto@gmail.com>
2025-07-10 13:48:47 -0300

    refactor: Modifies the attributes of the response character following the challenge requirements.

--- src/main/java/io/github/josiasbarreto_dev/desafio_naruto/dto/CharacterResponseDTO.java
+++ src/main/java/io/github/josiasbarreto_dev/desafio_naruto/dto/CharacterResponseDTO.java
@@ -1,14 +1,15 @@
 package io.github.josiasbarreto_dev.desafio_naruto.dto;
 
+import io.github.josiasbarreto_dev.desafio_naruto.model.Jutsu;
 import io.github.josiasbarreto_dev.desafio_naruto.model.NinjaType;
 
 import java.util.ArrayList;
+import java.util.Map;
 
 public record CharacterResponseDTO(
         Long id,
         String name,
-        int age,
-        String village,
-        ArrayList<String> jutsus,
+        Map<String, Jutsu> jutsus,
         int chakra,
-        NinjaType ninjaType) {}
+        int life
+) {}
