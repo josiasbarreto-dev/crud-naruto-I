45739c10551c4039864b2c87c87b82b1ccdb6553
Josias Barreto <sr.josiasbarreto@gmail.com>
2025-07-10 12:39:22 -0300

    feat: Created an exception to be thrown when searching for jutsu that does not exist and for insufficient chakra when performing battle.

--- src/main/java/io/github/josiasbarreto_dev/desafio_naruto/exception/GlobalExceptionHandler.java
+++ src/main/java/io/github/josiasbarreto_dev/desafio_naruto/exception/GlobalExceptionHandler.java
@@ -13,7 +13,7 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(ResourceNotFoundException.class)
     public ResponseEntity<Map<String, Object>> handleResourceNotFoundException(ResourceNotFoundException ex) {
         Map<String, Object> response = new HashMap<>();
-        response.put("status", HttpStatus.NOT_FOUND.value()); // 404
+        response.put("status", HttpStatus.NOT_FOUND.value());
         response.put("error", "Not Found");
         response.put("message", ex.getMessage());
 
@@ -28,5 +28,23 @@ public class GlobalExceptionHandler {
         response.put("message", ex.getMessage());
 
         return ResponseEntity.status(HttpStatus.CONFLICT).body(response);
+    }
+
+    @ExceptionHandler(JutsuNotFoundException.class)
+    public ResponseEntity<Map<String, Object>> handleJutsuNotFoundException(JutsuNotFoundException ex) {
+        Map<String, Object> body = new HashMap<>();
+        body.put("status", HttpStatus.BAD_REQUEST.value());
+        body.put("error", HttpStatus.BAD_REQUEST.getReasonPhrase());
+        body.put("message", ex.getMessage());
+        return new ResponseEntity<>(body, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(InsufficientChakraException.class)
+    public ResponseEntity<Map<String, Object>> handleInsufficientChakraException(InsufficientChakraException ex) {
+        Map<String, Object> body = new HashMap<>();
+        body.put("status", HttpStatus.BAD_REQUEST.value());
+        body.put("error", HttpStatus.BAD_REQUEST.getReasonPhrase());
+        body.put("message", ex.getMessage());
+        return new ResponseEntity<>(body, HttpStatus.BAD_REQUEST);
     }
 }
---
+++ src/main/java/io/github/josiasbarreto_dev/desafio_naruto/exception/JutsuNotFoundException.java
@@ -0,0 +1,7 @@
+package io.github.josiasbarreto_dev.desafio_naruto.exception;
+
+public class JutsuNotFoundException extends RuntimeException{
+    public JutsuNotFoundException(String message) {
+        super(message);
+    }
+}
