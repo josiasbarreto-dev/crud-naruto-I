904c3a7071ffef3bc20bd6bb1602e5ed3e4f7141
Josias Barreto <sr.josiasbarreto@gmail.com>
2025-07-16 22:23:21 -0300

    feat: Adds handler to receive validation exceptions and returns a friendlier error message.

--- src/main/java/io/github/josiasbarreto_dev/desafio_naruto/exception/GlobalExceptionHandler.java
+++ src/main/java/io/github/josiasbarreto_dev/desafio_naruto/exception/GlobalExceptionHandler.java
@@ -2,6 +2,8 @@ package io.github.josiasbarreto_dev.desafio_naruto.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -10,6 +12,25 @@ import java.util.Map;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, Object>> handleValidationExceptions(
+            MethodArgumentNotValidException ex) {
+
+        Map<String, String> fieldErrors = new HashMap<>();
+        ex.getBindingResult().getAllErrors().forEach(error -> {
+            String fieldName = error instanceof FieldError ? ((FieldError) error).getField() : error.getObjectName();
+            String errorMessage = error.getDefaultMessage();
+            fieldErrors.put(fieldName, errorMessage);
+        });
+
+        Map<String, Object> response = new HashMap<>();
+        response.put("status", HttpStatus.BAD_REQUEST.value()); // 400
+        response.put("error", "Bad Request");
+        response.put("message", "Validation failed for one or more fields.");
+        response.put("details", fieldErrors);
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
+    }
     @ExceptionHandler(ResourceNotFoundException.class)
     public ResponseEntity<Map<String, Object>> handleResourceNotFoundException(ResourceNotFoundException ex) {
         Map<String, Object> response = new HashMap<>();
