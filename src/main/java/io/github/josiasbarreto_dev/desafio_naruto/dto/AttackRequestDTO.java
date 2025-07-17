00860bffff1ba5e053d045906db2c4b870e8e7b2
Josias Barreto <sr.josiasbarreto@gmail.com>
2025-07-16 22:19:43 -0300

    feat: Adds validation to the attributes of the AttackRequestDTO class.

--- src/main/java/io/github/josiasbarreto_dev/desafio_naruto/dto/AttackRequestDTO.java
+++ src/main/java/io/github/josiasbarreto_dev/desafio_naruto/dto/AttackRequestDTO.java
@@ -1,4 +1,22 @@
 package io.github.josiasbarreto_dev.desafio_naruto.dto;
 
-public record AttackRequestDTO(String attacker, String target, String jutsu) {
-}
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+
+public record AttackRequestDTO(
+        @NotBlank(message = "Attacker is required.")
+        @Size(min = 3, max = 50, message = "Name must be between 3 and 50 characters.")
+        @Pattern(regexp = "^[A-Z]+(.)*", message = "Name must start with an uppercase letter and can only contain letters and spaces.")
+        String attacker,
+
+        @NotBlank(message = "Target is required.")
+        @Size(min = 3, max = 50, message = "Name must be between 3 and 50 characters.")
+        @Pattern(regexp = "^[A-Z]+(.)*", message = "Name must start with an uppercase letter and can only contain letters and spaces.")
+        String target,
+
+        @NotBlank(message = "Jutsu is required.")
+        @Size(min = 3, max = 50, message = "Jutsu name must be between 3 and 50 characters.")
+        @Pattern(regexp = "^[A-Z]+(.)*", message = "Name must start with an uppercase letter and can only contain letters and spaces.")
+        String jutsu
+){}
