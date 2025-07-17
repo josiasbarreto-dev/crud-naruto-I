967b90da87bd2b7ad4bbd8196512dd31bb337765
Josias Barreto <sr.josiasbarreto@gmail.com>
2025-07-16 22:18:58 -0300

    feat: Adds validation to the attributes of the JutsuRequestDTO class.

--- src/main/java/io/github/josiasbarreto_dev/desafio_naruto/dto/JutsuRequestDTO.java
+++ src/main/java/io/github/josiasbarreto_dev/desafio_naruto/dto/JutsuRequestDTO.java
@@ -1,4 +1,14 @@
 package io.github.josiasbarreto_dev.desafio_naruto.dto;
 
-public record JutsuRequestDTO(int damage, int chakraConsumption) {
-}
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+
+public record JutsuRequestDTO(
+        @NotNull(message = "Damage is required.")
+        @Positive(message = "Damage must be a positive integer.")
+        Integer damage,
+
+        @NotNull(message = "Chakra consumption is required.")
+        @Positive(message = "Chakra consumption must be a positive integer.")
+        Integer chakraConsumption
+){}
