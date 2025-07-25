5a54ea6a27106ddedb8576ce5fd842a449af7c2a
Josias Barreto <sr.josiasbarreto@gmail.com>
2025-07-25 19:01:59 -0300

    refactor: Modifies useJutsu() by separating the Jutsu class into a new entity and adds a new getType() method.

--- src/main/java/io/github/josiasbarreto_dev/desafio_naruto/model/NinjutsuNinja.java
+++ src/main/java/io/github/josiasbarreto_dev/desafio_naruto/model/NinjutsuNinja.java
@@ -18,12 +18,17 @@ public class NinjutsuNinja extends Character {
 
     @Override
     public void useJutsu(String jutsuName, Character adversaryNinja){
-        Jutsu jutsu = jutsus.get(jutsuName);
-        if(jutsu == null){
-            throw new JutsuNotFoundException(name + " doesn't know the jutsu: " + jutsuName);
-        }
-        if(chakra < jutsu.getChakraConsumption()){
-            throw new InsufficientChakraException(name + " doesn't have enough chakra to use " + jutsuName + ". Required: " + jutsu.getChakraConsumption() + ", Available: " + chakra);
+        Jutsu jutsu = jutsus.stream()
+                .filter(j -> j.getName().equalsIgnoreCase(jutsuName))
+                .findFirst()
+                .orElseThrow(() -> new JutsuNotFoundException(
+                        name + " doesn't know the jutsu: " + jutsuName
+                ));
+
+        if (chakra < jutsu.getChakraConsumption()) {
+            throw new InsufficientChakraException(
+                    name + " doesn't have enough chakra to use " + jutsuName
+            );
         }
 
         chakra -= jutsu.getChakraConsumption();
@@ -39,5 +44,10 @@ public class NinjutsuNinja extends Character {
             loseLife(jutsu.getDamage());
             System.out.println(name + " got hit and lost " + jutsu.getDamage() + " of life.");
         }
+    }
+
+    @Override
+    public NinjaType getType() {
+        return NinjaType.NINJUTSU;
     }
 }
