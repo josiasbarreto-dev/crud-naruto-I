873050895a0bc5222a3b9e6c4cfe036ab5a3674a
Josias Barreto <sr.josiasbarreto@gmail.com>
2025-07-25 18:06:32 -0300

    refactor: Separates the Jutsus into a separate entity and uses a Jutsu List to associate with the character.

--- src/test/java/io/github/josiasbarreto_dev/desafio_naruto/controller/CharacterControllerTest.java
+++ src/test/java/io/github/josiasbarreto_dev/desafio_naruto/controller/CharacterControllerTest.java
@@ -3,7 +3,6 @@ package io.github.josiasbarreto_dev.desafio_naruto.controller;
 import io.github.josiasbarreto_dev.desafio_naruto.controller.impl.CharacterController;
 import io.github.josiasbarreto_dev.desafio_naruto.dto.*;
 import io.github.josiasbarreto_dev.desafio_naruto.exception.ResourceNotFoundException;
-import io.github.josiasbarreto_dev.desafio_naruto.model.Jutsu;
 import io.github.josiasbarreto_dev.desafio_naruto.model.NinjaType;
 import io.github.josiasbarreto_dev.desafio_naruto.service.impl.CharacterService;
 import org.junit.jupiter.api.BeforeEach;
@@ -17,7 +16,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
 import java.util.List;
-import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
@@ -25,12 +23,23 @@ import static org.mockito.Mockito.*;
 @ExtendWith(MockitoExtension.class)
 @DisplayName("Controller Unit Tests")
 public class CharacterControllerTest {
+    private static final Long VALID_CHARACTER_ID = 1L;
+    private static final Long INVALID_CHARACTER_ID = 10L;
+    private static final NinjaType NINJA_TYPE_TAIJUTSU = NinjaType.TAIJUTSU;
+    private static final Integer DEFAULT_CHAKRA = 100;
+    private static final Integer DEFAULT_LIFE = 100;
+    private static final Integer NARUTO_CHAKRA = 150;
+    private static final Integer NARUTO_LIFE = 200;
+    private static final Integer DAMAGE_HIGH = 80;
+    private static final Integer CHAKRA_COST_HIGH = 60;
+    private static final Integer DAMAGE_MEDIUM = 20;
+    private static final Integer CHAKRA_COST_MEDIUM = 30;
+    private static final Integer DAMAGE_ZERO = 0;
+    private static final Integer CHAKRA_COST_FULL = 100;
     @InjectMocks
     private CharacterController characterController;
-
     @Mock
     private CharacterService characterService;
-
     private CharacterRequestDTO payloadRequestDTO;
     private CharacterRequestDTO payloadRequestWithNinjaTypeInvalid;
     private CharacterResponseDTO expectedCharacterJiraiya;
@@ -38,22 +47,6 @@ public class CharacterControllerTest {
     private List<CharacterResponseDTO> characterListResponseDTO;
     private AttackRequestDTO requestAttackDTO;
     private BattleResponseDTO battleResponseDTO;
-
-    private static final Long VALID_CHARACTER_ID = 1L;
-    private static final Long INVALID_CHARACTER_ID = 10L;
-    private static final NinjaType NINJA_TYPE_TAIJUTSU = NinjaType.TAIJUTSU;
-
-    private static final Integer DEFAULT_CHAKRA = 100;
-    private static final Integer DEFAULT_LIFE = 100;
-    private static final Integer NARUTO_CHAKRA = 150;
-    private static final Integer NARUTO_LIFE = 200;
-
-    private static final Integer DAMAGE_HIGH = 80;
-    private static final Integer CHAKRA_COST_HIGH = 60;
-    private static final Integer DAMAGE_MEDIUM = 20;
-    private static final Integer CHAKRA_COST_MEDIUM = 30;
-    private static final Integer DAMAGE_ZERO = 0;
-    private static final Integer CHAKRA_COST_FULL = 100;
 
     @BeforeEach
     void setup() {
@@ -70,27 +63,27 @@ public class CharacterControllerTest {
         battleResponseDTO = buildBattleResponse(expectedCharacterJiraiya, expectedCharacterNaruto);
     }
 
-    private Map<String, Jutsu> getDefaultJutsus() {
-        return Map.of(
-                "Rasengan", new Jutsu(DAMAGE_HIGH, CHAKRA_COST_HIGH),
-                "Summoning Jutsu: Toad", new Jutsu(DAMAGE_MEDIUM, CHAKRA_COST_MEDIUM),
-                "Sage Mode", new Jutsu(DAMAGE_ZERO, CHAKRA_COST_FULL)
+    private List<JutsuResponseDTO> getDefaultJutsus() {
+        return List.of(
+                new JutsuResponseDTO(1L, "Rasengan", DAMAGE_HIGH, CHAKRA_COST_HIGH),
+                new JutsuResponseDTO(2L, "Summoning Jutsu: Toad", DAMAGE_MEDIUM, CHAKRA_COST_MEDIUM),
+                new JutsuResponseDTO(3L, "Sage Mode", DAMAGE_ZERO, CHAKRA_COST_FULL)
         );
     }
 
-    private Map<String, JutsuRequestDTO> getDefaultJutsusRequest() {
-        return Map.of(
-                "Rasengan", new JutsuRequestDTO(DAMAGE_HIGH, CHAKRA_COST_HIGH),
-                "Summoning Jutsu: Toad", new JutsuRequestDTO(DAMAGE_MEDIUM, CHAKRA_COST_MEDIUM),
-                "Sage Mode", new JutsuRequestDTO(DAMAGE_ZERO, CHAKRA_COST_FULL)
+    private List<JutsuRequestDTO> getDefaultJutsusRequest() {
+        return List.of(
+                new JutsuRequestDTO("Rasengan", DAMAGE_HIGH, CHAKRA_COST_HIGH),
+                new JutsuRequestDTO("Summoning Jutsu: Toad", DAMAGE_MEDIUM, CHAKRA_COST_MEDIUM),
+                new JutsuRequestDTO("Sage Mode", DAMAGE_ZERO, CHAKRA_COST_FULL)
         );
     }
 
-    private CharacterRequestDTO buildCharacterRequestDTO(String name, Map<String, JutsuRequestDTO> jutsus, Integer life, NinjaType type) {
+    private CharacterRequestDTO buildCharacterRequestDTO(String name, List<JutsuRequestDTO> jutsus, Integer life, NinjaType type) {
         return new CharacterRequestDTO(name, jutsus, life, type);
     }
 
-    private CharacterResponseDTO buildCharacterResponseDTO(Long id, String name, Map<String, Jutsu> jutsus, Integer chakra, Integer life) {
+    private CharacterResponseDTO buildCharacterResponseDTO(Long id, String name, List<JutsuResponseDTO> jutsus, Integer chakra, Integer life) {
         return new CharacterResponseDTO(id, name, jutsus, chakra, life);
     }
 
@@ -129,7 +122,7 @@ public class CharacterControllerTest {
 
     @Test
     @DisplayName("Deve listar todos os Personagens e retornar o Status Code 200")
-    void shouldListAllCharactersAndReturnOkStatus(){
+    void shouldListAllCharactersAndReturnOkStatus() {
         when(characterService.listCharacter()).thenReturn(characterListResponseDTO);
 
         ResponseEntity<List<CharacterResponseDTO>> response = characterController.list();
@@ -143,7 +136,7 @@ public class CharacterControllerTest {
 
     @Test
     @DisplayName("Deve retornar um Ninja específico com status OK quando encontrado")
-    void shouldReturnCharacterByIdAnd200OkStatus(){
+    void shouldReturnCharacterByIdAnd200OkStatus() {
         when(characterService.getCharacterById(VALID_CHARACTER_ID)).thenReturn(expectedCharacterJiraiya);
 
         ResponseEntity<CharacterResponseDTO> response = characterController.get(VALID_CHARACTER_ID);
@@ -171,7 +164,7 @@ public class CharacterControllerTest {
 
     @Test
     @DisplayName("Deve listar todos os Personagens com o Ninja Type Informado e retornar o Status Code 200")
-    void shouldListCharactersByNinjaTypeAndReturnOkStatus(){
+    void shouldListCharactersByNinjaTypeAndReturnOkStatus() {
         when(characterService.listCharactersByType(NINJA_TYPE_TAIJUTSU)).thenReturn(characterListResponseDTO);
 
         ResponseEntity<List<CharacterResponseDTO>> response = characterController.list(NINJA_TYPE_TAIJUTSU);
@@ -185,7 +178,7 @@ public class CharacterControllerTest {
 
     @Test
     @DisplayName("Deve deletar Personagem com sucesso e retornar 204 No Content")
-    void shouldDeleteCharacterSuccessfullyAndReturn204NoContent(){
+    void shouldDeleteCharacterSuccessfullyAndReturn204NoContent() {
         doNothing().when(characterService).deleteCharacterById(VALID_CHARACTER_ID);
 
         ResponseEntity<Void> response = characterController.delete(VALID_CHARACTER_ID);
@@ -197,7 +190,7 @@ public class CharacterControllerTest {
 
     @Test
     @DisplayName("Deve lançar ResourceNotFoundException ao tentar deletar Personagem inexistente")
-    void shouldThrowResourceNotFoundExceptionWhenDeletingNonExistentCharacter(){
+    void shouldThrowResourceNotFoundExceptionWhenDeletingNonExistentCharacter() {
         String message = "Ninja with id " + INVALID_CHARACTER_ID + " not found.";
         doThrow(new ResourceNotFoundException(message))
                 .when(characterService).deleteCharacterById(INVALID_CHARACTER_ID);
@@ -209,9 +202,10 @@ public class CharacterControllerTest {
         assertEquals(message, exception.getMessage());
         verifyNoMoreInteractions(characterService);
     }
+
     @Test
     @DisplayName("Deve adicionar o novo chakra e retornar sucesso")
-    void shouldAddNewChakraAndReturnSuccess(){
+    void shouldAddNewChakraAndReturnSuccess() {
         Integer chakraAmount = 100;
 
         when(characterService.addChakra(VALID_CHARACTER_ID, chakraAmount)).thenReturn(expectedCharacterJiraiya);
@@ -226,7 +220,7 @@ public class CharacterControllerTest {
 
     @Test
     @DisplayName("Deve retornar Resultado da Batalha e Status 200 OK quando a Luta Ocorre.")
-    void shouldReturnBattleResultAnd200OkStatusWhenFightOccurs(){
+    void shouldReturnBattleResultAnd200OkStatusWhenFightOccurs() {
         when(characterService.fight(requestAttackDTO)).thenReturn(battleResponseDTO);
 
         ResponseEntity<BattleResponseDTO> response = characterController.fight(requestAttackDTO);
