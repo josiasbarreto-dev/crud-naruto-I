21ab86e032359113bee10363be0558c825bd4121
Josias Barreto <sr.josiasbarreto@gmail.com>
2025-07-25 19:07:57 -0300

    refactor: Refactor the mapper to only convert characters.

--- src/main/java/io/github/josiasbarreto_dev/desafio_naruto/mapper/CharacterMapper.java
+++ src/main/java/io/github/josiasbarreto_dev/desafio_naruto/mapper/CharacterMapper.java
@@ -1,21 +1,16 @@
 package io.github.josiasbarreto_dev.desafio_naruto.mapper;
 
 import io.github.josiasbarreto_dev.desafio_naruto.dto.CharacterResponseDTO;
-import io.github.josiasbarreto_dev.desafio_naruto.dto.JutsuRequestDTO;
 import io.github.josiasbarreto_dev.desafio_naruto.model.Character;
-import io.github.josiasbarreto_dev.desafio_naruto.model.Jutsu;
 import org.mapstruct.Mapper;
 import org.mapstruct.factory.Mappers;
 
 import java.util.List;
 
-@Mapper(componentModel = "spring")
+@Mapper(componentModel = "spring", uses = {JutsuMapper.class})
 public interface CharacterMapper {
     CharacterMapper INSTANCE = Mappers.getMapper(CharacterMapper.class);
 
     CharacterResponseDTO toDTO(Character character);
-    List<CharacterResponseDTO> toDTO(List<Character> characters);
-
-    Jutsu toEntity(JutsuRequestDTO jutsuRequestDTO);
-    JutsuRequestDTO toDTO(Jutsu jutsu);
+    List<CharacterResponseDTO> toDTOCharacters(List<Character> characters);
 }
